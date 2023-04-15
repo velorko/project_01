2bef3a9389d88c8781d86174fd23b798b62c32e1
velorko <bolotovavd@gmail.com>
2023-04-15 23:13:32 +0300

    task_1.1 Done!

--- homeworks_lvl_1/task_1.1.py
+++ homeworks_lvl_1/task_1.1.py
@@ -1,8 +1,22 @@
 # Задача 1.1.
 
 # Есть строка с перечислением песен
-
 my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
 
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
-# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+# Первый трек
+first_song = my_favorite_songs.split(',')[0].strip()
+print('Первый трек: ', first_song)
+
+# Последний трек
+last_song = my_favorite_songs.split(',')[-1].strip()
+print('Последний трек: ', last_song)
+
+# Второй трек
+second_song = my_favorite_songs.split(',')[1].strip()
+print('Второй трек: ', second_song)
+
+# Второй с конца трек
+second_to_last_song = my_favorite_songs.split(',')[-2].strip()
+print('Второй с конца трек: ', second_to_last_song)
