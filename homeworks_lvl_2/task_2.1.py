0d76fa9857168e2ba0408a5c67be34cbd805db22
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-24 16:40:18 +0300

    Выполнено

--- homeworks_lvl_2/task_2.1.py
+++ homeworks_lvl_2/task_2.1.py
@@ -55,3 +55,30 @@ arr4 = [5]
 print("arr4:", arr4)
 print("Minimum:", minimum(arr4))  # 5
 print("Maximum:", maximum(arr4))  # 5
+
+# Супер!) 
+# есть вариант использовать другой алгоритм сортировки. Например быструю
+def quicksort(arr):
+    if len(arr) < 2:
+        # базовый случай, массив с 0 или 1 элементом уже отсортирован
+        return arr
+    else:
+        # рекурсивный случай. выберем опорный элемент pivot
+        pivot = arr[0]
+        # подмассив всех элементов, меньших, чем опорный элемент pivot
+        less = [i for i in arr[1:] if i <= pivot]
+        # подмассив всех элементов, превышающих опорный элемент pivot
+        greater = [i for i in arr[1:] if i > pivot]
+        # обединяем в порядке "сортировка для меньшего подмассива – опорный элемент – сортировка для большего подмассив"
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+# Максимум - это последний элемент отсортированного списка
+def maximum(arr):
+    return quicksort(arr)[-1]
+
+ # Минимум - это первый элемент отсортированного списка
+def minimum(arr):
+    return quicksort(arr)[0]
+print('\nБыстрая сортировка')
+print('min =', minimum([4,6,2,1,9,63,-134,566]))
+print('max =', maximum([4,6,2,1,9,63,-134,566]))
