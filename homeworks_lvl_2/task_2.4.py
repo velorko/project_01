381afcd012d1cc670c4741bfca24299a9b6d7dcd
velorko <bolotovavd@gmail.com>
2023-05-01 18:38:35 +0300

    task_2.4.py Done!

--- homeworks_lvl_2/task_2.4.py
+++ homeworks_lvl_2/task_2.4.py
@@ -8,8 +8,10 @@
 # foo("Oh, no!!!") -> "Oh, no"
 
 def remove_exclamation_marks(s):
-    pass
-
+    return s.replace('!', '')
+print("Пункт A. Входные данные: ""Hi! Hello!"" Выходные данные: ",remove_exclamation_marks ("Hi! Hello!"))
+print("Пункт A. Входные данные: "" "" Выходные данные: ",remove_exclamation_marks (" "))
+print("Пункт A. Входные данные: ""Oh, no!!!"" Выходные данные: ",remove_exclamation_marks ("Oh, no!!!"))
 
 # Пункт B.
 # Удалите восклицательный знак из конца строки. 
@@ -18,9 +20,13 @@ def remove_exclamation_marks(s):
 # remove("!Hi") == "!Hi"
 
 def remove_last_em(s):
-    pass
-
-
+    if s.endswith('!'):
+        return s[:-1] + s[-1:].replace('!', '', 1)
+    else:
+        return s
+print("Пункт B. Входные данные: ""Hi!"" Выходные данные: ",remove_last_em ("Hi!"))
+print("Пункт B. Входные данные: ""Hi!!!"" Выходные данные: ",remove_last_em ("Hi!!!"))
+print("Пункт B. Входные данные: ""!Hi"" Выходные данные: ",remove_last_em ("!Hi"))
 # Дополнительно
 
 # Пункт С.
@@ -36,4 +42,17 @@ def remove_last_em(s):
 # remove("Hi! !Hi! Hi!") === "!Hi!"
 
 def remove_word_with_one_em(s):
-    pass
+    words = s.split()
+    new_words = []
+    for word in words:
+        if word.count('!') != 1:
+            new_words.append(word)
+    return ' '.join(new_words)
+
+print("Пункт C. Входные данные: ""Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi!"))
+print("Пункт C. Входные данные: ""Hi! Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi! Hi!"))
+print("Пункт C. Входные данные: ""Hi! Hi! Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi! Hi! Hi!"))
+print("Пункт C. Входные данные: ""Hi Hi! Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi Hi! Hi!"))
+print("Пункт c. Входные данные: ""Hi! !Hi Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi! !Hi Hi!"))
+print("Пункт C. Входные данные: ""Hi! Hi!! Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi! Hi!! Hi!"))
+print("Пункт C. Входные данные: ""Hi! !Hi! Hi!"" Выходные данные: ",remove_word_with_one_em ("Hi! !Hi! Hi!"))
