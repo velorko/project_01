5118d816dcca646dadc9ad3344466ab11e8b6d53
velorko <bolotovavd@gmail.com>
2023-05-01 19:38:04 +0300

    task_3.1.py Done!

--- homeworks_lvl_3/task_3.1.py
+++ homeworks_lvl_3/task_3.1.py
@@ -25,3 +25,59 @@
 #   - отображать в таблице/матрице название колонки не обязательно!
 #   - проявите фантазию :)
 
+# Создаем  класс "Matrix" который содержит две перменные - количество строк и количество 
+# класс имеет  методы "set_value" для установки значения в ячейку матрицы 
+# и "get_value" для получения значения из ячейки матрицы
+class Matrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = [[None] * columns for _ in range(rows)]
+
+    def set_value(self, row, column, value):
+        if row < 0 or row >= self.rows or column < 0 or column >= self.columns:
+            raise IndexError("Index out of range")
+        self.matrix[row][column] = value
+
+    def get_value(self, row, column):
+        if row < 0 or row >= self.rows or column < 0 or column >= self.columns:
+            raise IndexError("Index out of range")
+        return self.matrix[row][column]
+
+    def num_rows(self):
+        return self.rows
+
+    def num_columns(self):
+        return self.columns
+
+    
+ #метод для отображения матрицы на экране
+    def print(self):
+        for i in range(self.rows):
+            row = "["
+            for j in range(self.columns):
+                value = str(self.get_value(i, j))
+                if value is None:
+                    value = "None"
+                row += value + ", "
+            row = row[:-2] + "]"
+            print(row)
+
+# создаем матрицу 10x10 из единиц
+matrix = Matrix(10, 10)
+for i in range(10):
+    for j in range(10):
+        matrix.set_value(i, j, 1)
+
+# устанавливаем значение в ячейку (3, 5)
+matrix.set_value(3, 5, 42)
+
+# получаем значение из ячейки (3, 5)
+print(matrix.get_value(3, 5)) # 42
+
+# выводим количество строк и столбцов матрицы
+print(matrix.num_rows()) # 10
+print(matrix.num_columns()) # 10
+# выводим матрицу на экран 
+print(matrix.print())
+
